396b29c2a7376882a651c843d8bd38dc41272d3a
Andrey Kondratyuk <andrey.vw@gmail.com>
2021-06-25 22:33:19 -0400

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         14: '3.5rem',
       },
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['system-ui', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         blue: colors.sky,
